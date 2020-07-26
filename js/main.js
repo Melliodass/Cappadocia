cfeef951b3470d73a4be17331ba7813c149e4198
unknown <jet333@yandex.ru>
2020-07-26 23:13:35 +0300

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+$(function () {
+  $('.fairy-tail__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
+    autoplay: true,
+    fade: true,
+    speed: 900,
+  });
+  $('.our-trip__slider').slick({
+    prevArrow: '<button type="button" class="slick-prev slick-btn"></button>',
+    nextArrow: '<button type="button" class="slick-next slick-btn"></button>',
+    autoplay: true,
+    fade: true,
+    speed: 900,
+  });
+
+
+});
